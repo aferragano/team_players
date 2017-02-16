4289a0dea5635897900df68e1e48deb6abf8c1d1
Anthony Ferragano <aferragano@gmail.com>
2017-02-15 20:14:05 -0500

    last commit on npm. switching to yarn.

--- imports/ui/Team-list.jsx
+++ imports/ui/Team-list.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import { ListItem } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
+import ActionDeleteForever from 'material-ui/svg-icons/action/delete-forever';
 
 export default class TeamList extends Component {
 	render() {
