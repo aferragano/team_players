04d86c8c36c89091d73b96a37287b86db71eb84d
Anthony Ferragano <aferragano@gmail.com>
2017-02-17 14:05:19 -0500

    hooking meteor up with the front

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,31 +1,29 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
 import { List } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Link } from 'react-router';
+import { Meteor } from 'meteor/meteor';
+
+//database collection 
+import { Persons } from '../api/persons';
 
 import Person from './Person';
 import TeamStats from './Team-stats';
 import TeamList from './Team-list';
 
-export default class App extends Component {
+export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 
 		//setting up the state
 		this.state = { persons: []};
 	}
-	componentWillMount() {
-		this.setState({ persons: [
-				{
-					_id: 1,
-					nickName: "Mr. Fahrenheit"
-				}
-				]});
-				}
 	renderPersons() {
-    return this.state.persons.map((person) => (
+    return this.props.persons.map((person) => (
       <TeamList key={person._id} person={person} />
     ));
   }
@@ -42,7 +40,8 @@ export default class App extends Component {
 
 						<div className="col s12 m7"> <Person/> </div>
 						<div className="col s12 m5"> 
-						<h2>Team List</h2>
+						<h2>Team List</h2> 
+						<Link to='/new' className="waves-effect wave-light btn">Add</Link>
 							<Divider/> 
 								<List>
 									{this.renderPersons()}
@@ -56,3 +55,14 @@ export default class App extends Component {
 			)
 	}
 }
+
+App.propTypes = {
+	persons: PropTypes.array.isRequired,
+};
+
+// export default createContainer(() => {
+// 	Meteor.subscribe('persons');
+// 	return {
+// 		persons: Persons.find({}, {sort:{ name: 1}}).fetch(),
+// 	};
+// }, App)
