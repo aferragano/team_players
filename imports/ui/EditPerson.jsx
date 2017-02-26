4f330c1d20612a2180ddef175f2e7ff90f6fb6f1
Anthony Ferragano <aferragano@gmail.com>
2017-02-26 10:54:23 -0500

    crud complete - mving on to chart

--- imports/ui/EditPerson.jsx
+++ imports/ui/EditPerson.jsx
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import {browserHistory} from 'react-router';
-import { Persons } from '../api/persons'
 
-export default class New extends Component {
-	submitPerson(e){
+export default class Edit extends Component {
+	showTeamStats(){
+		this.props.showTeamStats();
+	}
+
+	editPerson(e){
 		e.preventDefault();
 
-		let person = {
+			let person = {
+			_id: this.props.currentPerson._id,
 			nickname: this.refs.nickname.value,
 			team: this.refs.team.value,
 			notes: this.refs.notes.value,
@@ -14,34 +17,36 @@ export default class New extends Component {
 			createdAt: new Date(),
 		}
 		
-
-		Meteor.call('insertPerson', person, (error) => {
+		Meteor.call('updatePerson', person, (error)=> {
 			if(error) {
-        alert("Oups something went wrong: " + error.reason);
-      } else {
-        alert("Person added");
-        browserHistory.push('/');
-      }
-    });
+				alert("error occurred " + error.reason)
+			} else {
+				alert("person Updated ");
+				this.showTeamStats();
+			}
+		});
+
+		
 	}
-	render() {;
+	render() {
+		const currentPerson =this.props.currentPerson;
 		return (
 			<div className="row">
-				<form className="col s12" onSubmit={this.submitPerson.bind(this)}>
+				<form className="col s12" onSubmit={this.editPerson.bind(this)}>
 					<h3>Add Person</h3>
 					<div className="row">
 						<div className="input-field col s6">
-							<input placeholder="Nickname" ref="nickname" type="text" className="validate" />
+							<input placeholder="Nickname" ref="nickname" type="text" className="validate" defaultValue={currentPerson.name}/>
 						</div>
 						<div className="input-field col s6">
-							<input placeholder="Team" ref="team" type="text" className="validate" />
+							<input placeholder="Team" ref="team" type="text" className="validate" defaultValue={currentPerson.team}/>
 						</div>						
 					</div>
 
 					<div className="row">
 						<div className=" input-field col s6" >
 						<h5>communication</h5>
-							<select className="browser-default" ref="communication">
+							<select className="browser-default" ref="communication" defaultValue={currentPerson.communication}>
 								<option value="0"> 0 - sucks at it </option>
 								<option value="1"> 1 - eh at it </option>
 								<option value="2"> 2 - eh at it </option>
--- imports/ui/Team-list.jsx
+++ imports/ui/Team-list.jsx
@@ -3,18 +3,29 @@ import RaisedButton from 'material-ui/RaisedButton';
 import { ListItem } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import ActionDeleteForever from 'material-ui/svg-icons/action/delete-forever';
+import { red500 } from 'material-ui/styles/colors';
 
 export default class TeamList extends Component {
 	updateCurrentPerson(person){
 		this.props.updateCurrentPerson(person);
 	}
 
-
+	deletePerson(personId) {
+		Meteor.call('deletePerson', personId, (error) => {
+			if(error) {
+        alert("Oups something went wrong: " + error.reason);
+      } else {
+        console.log("deleted.....")
+      }
+    });
+	}
 	render() {
 		return (
 			<ListItem
 				primaryText={this.props.person.nickname}
 				leftAvatar={<Avatar src="city.jpg"/>}
+				rightIcon={<ActionDeleteForever hoverColor={red500} 
+					onClick={this.deletePerson.bind(this, this.props.person._id)}/>}
 				onClick={this.updateCurrentPerson.bind(this, this.props.person)}
 				/>
 			)
